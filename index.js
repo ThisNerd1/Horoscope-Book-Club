561ed74b6229c0cb5ad65a86fce9428bddd6a0fc
Daijah <daijahlinyahcoppage@gmail.com>
2022-03-01 20:21:38 -0700

    this doesn't work yet
    Node.js stuff.

---
+++ index.js
@@ -0,0 +1,15 @@
+// import express from "express";
+// import expressSession from 'express-session';
+// import path from "path";
+
+// const app = express();
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser());
+
+// let urlendcodedParser  = express.urlencoded({extended: false});
+
+
+// //nodemon
+
+// app.listen(3000);
