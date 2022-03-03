9d594b093e167df7b72fbcbff79c5c99adc60031
Daijah <daijahlinyahcoppage@gmail.com>
2022-03-02 18:30:33 -0700

    sign up stuff

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-// import express from "express";
-// import expressSession from 'express-session';
-// import path from "path";
+const express = require( "express");
+// const expressSession from 'express-session';
+const path = require( "path");
+const bodyParser = require("body-parser");
 
-// const app = express();
+const app = express();
 
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(cookieParser());
+app.set('views',__dirname+ '/views');
+// console.log(__dirname);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-// let urlendcodedParser  = express.urlencoded({extended: false});
-
-
-// //nodemon
-
-// app.listen(3000);
+let urlendcodedParser  = bodyParser.urlencoded({extended: false});
+app.use(express.static(path.join(__dirname , 'public')));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept')
+    next();
+});
+app.post("/loginAcc", urlendcodedParser, (req, res) => {
+    console.log(req.body.password);
+    console.log(req.body.email);
+    res.sendFile(__dirname + "/views/login.html")
+});
+app.post("/signUpAcc", urlendcodedParser, (req, res) => {
+    console.log(req.body.name);
+    console.log(req.body.birthday);
+    console.log(req.body.password);
+    console.log(req.body.email);
+    res.sendFile(__dirname + "/views/sign-up.html")
+});
+app.listen(3000);
---
+++ routes/routes.js
@@ -0,0 +1,62 @@
+import dateFormat, { masks } from "dateformat";
+import mongoose from "mongoose";
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb+srv://admin2:Pass112@cluster0.o4mbh.mongodb.net/test', {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true
+});
+
+let mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error'));
+mdb.once('open', callback => {});
+
+let userSchema = new mongoose.Schema({
+    username: String, //visible name that others see, defaults to same as username
+    password: String, //hashed one is saved
+    birthDate: Date,
+    starSign: String, 
+    moonSign: String,
+    risingSign: String
+    //[possibly other details for page customisation]
+}); 
+const users = mongoose.model('users', userSchema);
+
+//find one
+// users.findOne({starSign: 'zodiac1' }, function (err, docs) {
+//     if (err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log("Result : ", docs);
+//     }
+// });
+
+//delete one
+// users.deleteOne({ starSign: 'zodiac4' }).then(function(){
+//     console.log("Data deleted"); // Success
+// }).catch(function(error){
+//     console.log(error); // Failure
+// });
+
+// users.updateOne({
+
+// })
+
+
+// create an account
+// const User = new users({ 
+//     username: 'Username', 
+//     password: 'password', 
+//     birthDate: dateFormat('01-05-2001' ,"mm-dd-yyyy"),
+//     starSign: 'zodiac4',
+//     moonSign: 'zodiac5',
+//     risingSign: 'zodiac6'
+// });
+
+// User.save();
+// console.log(User);
+
+//This works it saves data to mongo compass.
+//terminal line ---node public/routes/routes.js
