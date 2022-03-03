fb2cfa820cae7da233e9c17997bb4e10669dde90
ethcala <81577564+ethcala@users.noreply.github.com>
2022-03-02 18:55:39 -0700

    merge

--- routes/routes.js
+++ routes/routes.js
@@ -45,13 +45,6 @@ const users = mongoose.model('users', userSchema);
 //     console.log(error); // Failure
 // });
 
-<<<<<<< HEAD
-// users.updateOne({
-
-// })
-
-=======
->>>>>>> a34474877b685b949df6a9c87e9b226f44a7cb19
 
 // create an account
 // const User = new users({ 
